20b93fdc31e6d311da9bee0aa70b2277e49fafd0
Nick Snyder <nsnyder@linkedin.com>
2016-07-13 08:09:31 -0700

    reset derived data

--- build.sh
+++ build.sh
@@ -2,18 +2,22 @@
 # Builds all targets and runs tests.
 
 set -o pipefail &&
-xcodebuild clean test \
+rm -rf /tmp/LayoutKit &&
+time xcodebuild clean test \
     -project LayoutKit.xcodeproj \
     -scheme LayoutKit-macOS \
     -sdk macosx10.11 \
+    -derivedDataPath /tmp/LayoutKit \
     OTHER_SWIFT_FLAGS='-Xfrontend -debug-time-function-bodies' \
     | tee build.log \
     | xcpretty
 cat build.log | sh debug-time-function-bodies.sh &&
-xcodebuild clean test \
+rm -rf /tmp/LayoutKit &&
+time xcodebuild clean test \
     -project LayoutKit.xcodeproj \
     -scheme LayoutKit-iOS \
     -sdk iphonesimulator9.3 \
+    -derivedDataPath /tmp/LayoutKit \
     -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3' \
     -destination 'platform=iOS Simulator,name=iPhone 6 Plus,OS=9.3' \
     -destination 'platform=iOS Simulator,name=iPhone 6,OS=8.4' \
@@ -22,10 +26,12 @@ xcodebuild clean test \
     | tee build.log \
     | xcpretty &&
 cat build.log | sh debug-time-function-bodies.sh &&
-xcodebuild clean build \
+rm -rf /tmp/LayoutKit &&
+time xcodebuild clean build \
     -project LayoutKit.xcodeproj \
     -scheme LayoutKitSampleApp-iOS \
     -sdk iphonesimulator9.3 \
+    -derivedDataPath /tmp/LayoutKit \
     -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3' \
     -destination 'platform=iOS Simulator,name=iPhone 6 Plus,OS=9.3' \
     -destination 'platform=iOS Simulator,name=iPhone 6,OS=8.4' \
