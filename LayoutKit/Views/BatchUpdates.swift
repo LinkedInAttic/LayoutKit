679856212e1526bac4c0b988c77c57d2788c3d50
Nick Snyder <nsnyder@linkedin.com>
2016-07-26 10:35:26 -0700

    remove unnecessary conformance to Hashable

--- LayoutKit/Views/BatchUpdates.swift
+++ LayoutKit/Views/BatchUpdates.swift
@@ -23,17 +23,13 @@ public struct BatchUpdates {
 }
 
 /// Instruction to move an item from one index path to another.
-public struct ItemMove: Hashable {
+public struct ItemMove: Equatable {
     public let from: NSIndexPath
     public let to: NSIndexPath
 
     public init(from: NSIndexPath, to: NSIndexPath) {
         self.from = from
         self.to = to
-    }
-
-    public var hashValue: Int {
-        return intHashValue(from.hashValue, to.hashValue)
     }
 }
 
@@ -42,7 +38,7 @@ public func ==(lhs: ItemMove, rhs: ItemMove) -> Bool {
 }
 
 /// Instruction to move a section from one index to another.
-public struct SectionMove: Hashable {
+public struct SectionMove: Equatable {
     public let from: Int
     public let to: Int
 
@@ -50,20 +46,8 @@ public struct SectionMove: Hashable {
         self.from = from
         self.to = to
     }
-
-    public var hashValue: Int {
-        return intHashValue(from, to)
-    }
 }
 
 public func ==(lhs: SectionMove, rhs: SectionMove) -> Bool {
     return lhs.from == rhs.from && lhs.to == rhs.to
-}
-
-/// Returns a hash value for the two integers
-/// by concatenating the lowest 32 bits of each integer.
-private func intHashValue(left: Int, _ right: Int) -> Int {
-    let leftLowBits = left % (1 << 32)
-    let rightLowBits = right % (1 << 32)
-    return leftLowBits + rightLowBits << 32
 }
