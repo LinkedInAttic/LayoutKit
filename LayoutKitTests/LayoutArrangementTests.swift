12ebbbbd84bd5ea80ad1b0c46860bae0a0a4685b
Nick Snyder <nsnyder@linkedin.com>
2016-10-17 22:48:16 -0700

    move PositionedLayoutTests into LayoutArrangementTests

--- LayoutKitTests/LayoutArrangementTests.swift
+++ LayoutKitTests/LayoutArrangementTests.swift
@@ -11,6 +11,25 @@ import LayoutKit
 
 class LayoutArrangementTests: XCTestCase {
 
+    func testMakeViewsSingle() {
+        let layout = SizeLayout<View>(width: 50, height: 50)
+        let view = layout.arrangement().makeViews()
+        XCTAssertTrue(view.subviews.isEmpty)
+        XCTAssertEqual(view.frame, CGRect(x: 0, y: 0, width: 50, height: 50))
+    }
+
+    func testMakeViewsMultiple() {
+        let insets = EdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        let layout1 = InsetLayout(insets: insets, sublayout: SizeLayout<View>(width: 50, height: 50) { _ in })
+        let layout2 = InsetLayout(insets: insets, sublayout: SizeLayout<View>(width: 50, height: 50) { _ in })
+        let stack = StackLayout(axis: .horizontal, sublayouts: [layout1, layout2])
+        let view = stack.arrangement(origin: CGPoint(x: 1, y: 2)).makeViews()
+        XCTAssertEqual(view.frame, CGRect(x: 1, y: 2, width: 140, height: 70))
+        XCTAssertEqual(view.subviews.count, 2)
+        XCTAssertEqual(view.subviews[0].frame, CGRect(x: 10, y: 10, width: 50, height: 50))
+        XCTAssertEqual(view.subviews[1].frame, CGRect(x: 80, y: 10, width: 50, height: 50))
+    }
+
     func testAnimation() {
 
         var redSquare: View? = nil
