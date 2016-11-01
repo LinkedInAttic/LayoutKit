c6dab29617fc1c7f7938c5b519c4c9babb700933
Nick Snyder <nsnyder@linkedin.com>
2016-11-01 14:42:01 -0700

    Rename LayoutKitExampleLayouts to ExampleLayouts so it isn't pulled in by Carthage
    These example layouts are not intended to be part of LayoutKit. They
    are intended as documentation and proof of concept.

--- LayoutKitSampleApp/ForegroundProfileViewController.swift
+++ LayoutKitSampleApp/ForegroundProfileViewController.swift
@@ -7,7 +7,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 
 import UIKit
-import LayoutKitExampleLayouts
+import ExampleLayouts
 
 class ForegroundMiniProfileViewController: UIViewController {
 
