8cbc14d175f9a2b3dd3589a54ea3b2cfc29cc3c8
Nick Snyder <nsnyder@linkedin.com>
2017-01-11 11:05:14 -0800

    Xcode 8.2

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ rm -rf $DERIVED_DATA &&
 time xcodebuild clean test \
     -project LayoutKit.xcodeproj \
     -scheme LayoutKit-tvOS \
-    -sdk appletvsimulator10.0 \
+    -sdk appletvsimulator10.1 \
     -derivedDataPath $DERIVED_DATA \
     -destination 'platform=tvOS Simulator,name=Apple TV 1080p,OS=9.2' \
     -destination 'platform=tvOS Simulator,name=Apple TV 1080p,OS=10.0' \
@@ -35,12 +35,14 @@ rm -rf $DERIVED_DATA &&
 time xcodebuild clean build \
     -project LayoutKit.xcodeproj \
     -scheme LayoutKitSampleApp \
-    -sdk iphonesimulator10.0 \
+    -sdk iphonesimulator10.2 \
     -derivedDataPath $DERIVED_DATA \
+    -destination 'platform=iOS Simulator,name=iPhone 6,OS=8.4' \
+    -destination 'platform=iOS Simulator,name=iPhone 6 Plus,OS=8.4' \
     -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3' \
     -destination 'platform=iOS Simulator,name=iPhone 6 Plus,OS=9.3' \
-    -destination 'platform=iOS Simulator,name=iPhone 6,OS=10.0' \
-    -destination 'platform=iOS Simulator,name=iPhone 6 Plus,OS=10.0' \
+    -destination 'platform=iOS Simulator,name=iPhone 6,OS=10.2' \
+    -destination 'platform=iOS Simulator,name=iPhone 6 Plus,OS=10.2' \
     OTHER_SWIFT_FLAGS='-Xfrontend -debug-time-function-bodies' \
     | tee build.log \
     | xcpretty &&
@@ -49,12 +51,14 @@ rm -rf $DERIVED_DATA &&
 time xcodebuild clean test \
     -project LayoutKit.xcodeproj \
     -scheme LayoutKit-iOS \
-    -sdk iphonesimulator10.0 \
+    -sdk iphonesimulator10.2 \
     -derivedDataPath $DERIVED_DATA \
+    -destination 'platform=iOS Simulator,name=iPhone 6,OS=8.4' \
+    -destination 'platform=iOS Simulator,name=iPhone 6 Plus,OS=8.4' \
     -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3' \
     -destination 'platform=iOS Simulator,name=iPhone 6 Plus,OS=9.3' \
-    -destination 'platform=iOS Simulator,name=iPhone 6,OS=10.0' \
-    -destination 'platform=iOS Simulator,name=iPhone 6 Plus,OS=10.0' \
+    -destination 'platform=iOS Simulator,name=iPhone 6,OS=10.2' \
+    -destination 'platform=iOS Simulator,name=iPhone 6 Plus,OS=10.2' \
     OTHER_SWIFT_FLAGS='-Xfrontend -debug-time-function-bodies' \
     | tee build.log \
     | xcpretty &&
