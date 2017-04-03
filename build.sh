a17f97b3ac94d9da3f2e5674fefa3a98ada09f55
Jingwei Huang <jghuang@linkedin.com>
2017-04-03 12:46:32 -0700

    Update swift version and travis build script (#113)
    * Update swift version and travis build script
    
    * update build scripts
    
    * Removed 10.1
    
    * Updated travis build script
    
    * updated travis build script
    
    * Add newlines which were addreeed by review

--- build.sh
+++ build.sh
@@ -7,8 +7,11 @@ echo "Derived data location: $DERIVED_DATA";
     #-destination 'platform=iOS Simulator,name=iPhone 6,OS=8.4' \
     #-destination 'platform=iOS Simulator,name=iPhone 6 Plus,OS=8.4' \
 
+
 set -o pipefail &&
 rm -rf $DERIVED_DATA &&
+
+# Run test on MacOS
 time xcodebuild clean test \
     -project LayoutKit.xcodeproj \
     -scheme LayoutKit-macOS \
@@ -19,41 +22,47 @@ time xcodebuild clean test \
     | xcpretty &&
 cat build.log | sh debug-time-function-bodies.sh &&
 rm -rf $DERIVED_DATA &&
+
+# Run test on tvOS
 time xcodebuild clean test \
     -project LayoutKit.xcodeproj \
     -scheme LayoutKit-tvOS \
-    -sdk appletvsimulator10.1 \
+    -sdk appletvsimulator10.2 \
     -derivedDataPath $DERIVED_DATA \
     -destination 'platform=tvOS Simulator,name=Apple TV 1080p,OS=9.2' \
-    -destination 'platform=tvOS Simulator,name=Apple TV 1080p,OS=10.0' \
+    -destination 'platform=tvOS Simulator,name=Apple TV 1080p,OS=10.2' \
     OTHER_SWIFT_FLAGS='-Xfrontend -debug-time-function-bodies' \
     | tee build.log \
     | xcpretty &&
 cat build.log | sh debug-time-function-bodies.sh &&
 rm -rf $DERIVED_DATA &&
+
+# Build sample app
 time xcodebuild clean build \
     -project LayoutKit.xcodeproj \
     -scheme LayoutKitSampleApp \
-    -sdk iphonesimulator10.2 \
+    -sdk iphonesimulator10.3 \
     -derivedDataPath $DERIVED_DATA \
     -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3' \
     -destination 'platform=iOS Simulator,name=iPhone 6 Plus,OS=9.3' \
-    -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.2' \
-    -destination 'platform=iOS Simulator,name=iPhone 7 Plus,OS=10.2' \
+    -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.3' \
+    -destination 'platform=iOS Simulator,name=iPhone 7 Plus,OS=10.3' \
     OTHER_SWIFT_FLAGS='-Xfrontend -debug-time-function-bodies' \
     | tee build.log \
     | xcpretty &&
 cat build.log | sh debug-time-function-bodies.sh &&
 rm -rf $DERIVED_DATA &&
+
+# Run test on iOS
 time xcodebuild clean test \
     -project LayoutKit.xcodeproj \
     -scheme LayoutKit-iOS \
-    -sdk iphonesimulator10.2 \
+    -sdk iphonesimulator10.3 \
     -derivedDataPath $DERIVED_DATA \
     -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3' \
     -destination 'platform=iOS Simulator,name=iPhone 6 Plus,OS=9.3' \
-    -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.2' \
-    -destination 'platform=iOS Simulator,name=iPhone 7 Plus,OS=10.2' \
+    -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.3' \
+    -destination 'platform=iOS Simulator,name=iPhone 7 Plus,OS=10.3' \
     OTHER_SWIFT_FLAGS='-Xfrontend -debug-time-function-bodies' \
     | tee build.log \
     | xcpretty &&
