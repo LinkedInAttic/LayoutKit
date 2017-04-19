c791b87f38fdb219e282182dc247436cfae28d6c
Jingwei Huang <jghuang@linkedin.com>
2017-04-19 16:51:20 -0700

    Removed assets from testing projects

--- Tests/cocoapods/tvos/LayoutKit-tvOS/AppDelegate.swift
+++ Tests/cocoapods/tvos/LayoutKit-tvOS/AppDelegate.swift
@@ -1,12 +1,13 @@
+// Copyright 2017 LinkedIn Corp.
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 //
-//  AppDelegate.swift
-//  LayoutKit-tvOS
-//
-//  Created by Jingwei Huang on 4/17/17.
-//  Copyright © 2017 Linkedin. All rights reserved.
-//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 
 import UIKit
+import LayoutKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
