9f72b60d9105431bd7ed2b902bad7f4cd7773788
Alexey Glushkov <soniccat@list.ru>
2017-12-23 20:18:12 +0300

    Add Cocoapods static_framework support

--- Sources/ViewRecycler.swift
+++ Sources/ViewRecycler.swift
@@ -6,7 +6,7 @@
 // software distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 
-import ObjectiveC
+import Foundation
 
 /**
  Provides APIs to recycle views by id.
