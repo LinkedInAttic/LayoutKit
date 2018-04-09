ce449093bf35fc601e9298050d7c5a0f2be22424
Kyle Sherman <ksherman@linkedin.com>
2018-04-09 16:03:01 -0700

    Adding ObjC wrapper for Animation in Swift

---
+++ Sources/ObjCSupport/LOKAnimation.swift
@@ -0,0 +1,26 @@
+// Copyright 2016 LinkedIn Corp.
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+@objc open class LOKAnimation: NSObject {
+    let animation: Animation
+
+    let arrangement: LayoutArrangement
+    let rootView: View
+    let direction: UserInterfaceLayoutDirection
+
+    init(animation: Animation) {
+        self.animation = animation
+        self.arrangement = animation.arrangement
+        self.rootView = animation.rootView
+        self.direction = animation.direction
+    }
+
+    @objc public func apply() {
+        animation.apply()
+    }
+}
