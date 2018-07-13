eede0832482e38aadda63a2a6a5a0013966de73d
Nitin Badakh <nbadakh@nbadakh-mn1.linkedin.biz>
2018-07-13 16:14:01 +0530

    Added direction support in `LOKLayoutArrangement` class
    - Added new method `makeViews(in:direction:)` in LOKLayoutArrangement` class.
    - This method takes `UserInterfaceLayoutDirection` as 2nd parameter.
    - So caller can send language direction to LOKLayoutArrangement similar to LayoutArrangement.

--- Sources/ObjCSupport/LOKLayoutArrangement.swift
+++ Sources/ObjCSupport/LOKLayoutArrangement.swift
@@ -38,8 +38,14 @@ import CoreGraphics
             height: height.isFinite ? height : nil))
     }
 
-    @objc public func makeViews(in view: View?) {
-        layoutArrangement.makeViews(in: view)
+    @discardableResult
+    @objc public func makeViews(in view: View?) -> View {
+        return layoutArrangement.makeViews(in: view)
+    }
+
+    @discardableResult
+    @objc public func makeViews(in view: View?, direction: UserInterfaceLayoutDirection) -> View {
+        return layoutArrangement.makeViews(in: view, direction: direction)
     }
 
     @objc public func makeViews() -> View {
